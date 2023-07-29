6aba15b9d2b8d913d5194d46a1c166b9e58e118f
pkemb <pkemb@outlook.com>
2023-07-29 23:03:48 +0800

    完善public.py的文档
    Signed-off-by: pkemb <pkemb@outlook.com>
    Change-Id: I869a6df697431f2c1a9a5745331b002f47495574

--- alist/public.py
+++ alist/public.py
@@ -13,9 +13,19 @@ class AlistPublic(object):
         self.settings = AlistPublicSettings(self.alist, self, self.endpoint)
 
     def path(self, path, page_num=1, page_size=30, password=None):
-        """ 根据路径和密码请求文件或文件列表。
-        :returns:
-            文件或目录属性，以及文件列表。
+        """
+        获取指定路径 path 下的文件和文件夹列表。
+
+        :param path: 路径
+        :param page_num: 默认为1
+        :param page_size: 默认为30
+        :param password: 路径的访问密码。
+        :return: 文件或目录属性，以及文件列表。
+
+        .. code-block:: python
+
+            files = client.public.path('/path')
+            print(files['files'])
         """
         endpoint = f'{self.endpoint}/path'
         data = {
@@ -28,9 +38,11 @@ class AlistPublic(object):
         return self.alist.post(endpoint, json=data)
 
     def preview(self, path):
-        """ 获取文件的预览URL。
-        :returns:
-            文件的预览URL。
+        """
+        获取文件的预览URL。
+
+        :param path: 文件路径
+        :return: 文件的预览URL。
         """
         endpoint = f'{self.endpoint}/preview'
         data = {
@@ -39,9 +51,12 @@ class AlistPublic(object):
         return self.alist.post(endpoint, json=data)
 
     def search(self, path, keyword):
-        """ 搜索文件。需要开启，默认是关闭的。
-        :returns:
-            搜索的文件列表。
+        """
+        搜索文件。需要开启设置 enable search。默认是关闭的。
+
+        :param path: 在此路径下搜索。
+        :param keyword: 搜索关键字
+        :return: 搜索的文件列表。
         """
         endpoint = f'{self.endpoint}/search'
         data = {
@@ -51,10 +66,20 @@ class AlistPublic(object):
         return self.alist.post(endpoint, json=data)
 
     def upload(self, files, path, password=None):
-        """ 上传文件到指定路径。
-        :returns:
-            上传结果。True表示成功，False表示失败。
         """
+        上传文件到指定路径。如果没有登录，则需要开启允许游客上传。参考meta。
+
+        :param files: 文件列表
+        :param path: 上传的路径
+        :param password: 访问密码
+        :type files: list
+        :return: 上传结果。True表示成功，False表示失败。
+
+        .. code-block:: python
+
+            client.public.upload(['/path/to/local_file'], '/path/to/upload/path')
+        """
+
         # Content Type 是 multipart/form-data
         endpoint = f'{self.endpoint}/upload'
         data = {
